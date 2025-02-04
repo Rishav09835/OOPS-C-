dd73df9a8aba287e436f35349d6fba8449097d1a
Rishav Dobriyal <73049840+Rishav09835@users.noreply.github.com>
2025-02-04 22:42:38 +0530

    added const keyword folder

---
+++ OOPS_C++/CONST keyword/constants.cpp
@@ -0,0 +1,17 @@
+    //const keyword are used to define constants and prevent modification of data. 
+    /*
+    const is used with:-
+    constant variables.
+    constant objects.
+    constant member functions.
+    constant function parameters.
+    constant pointers.
+    */#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //const variables -> prevents accidental modification of values.
+    const int x = 20;
+    // x=50; ->error, this can't be done.
+
+    return 0;
+}
